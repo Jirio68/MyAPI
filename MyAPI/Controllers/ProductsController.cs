9525c7164a87835dcf5f5f319f6b391d57168a15
Jirio68 <artemkobec02@gmail.com>
2025-04-01 20:58:34 +1000

    Initial commit

---
+++ MyAPI/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public ProductsController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            List<string> products = new List<string>();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                string query = "SELECT Author FROM Textbooks";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            products.Add(reader["Author"].ToString());
+                        }
+                    }
+                }
+            }
+            return products;
+        }
+    }
+}
